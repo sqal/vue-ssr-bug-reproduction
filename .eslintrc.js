308669e0c515fb0722fa6fa726bd2a25b4e72e57
Patryk Puszczyk <sqal08@gmail.com>
2018-10-22 18:22:27 +0200

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    message(vm) {
+      // works:
+      // return this.$store.state.message;
+      // does not work:
+      return vm.$store.state.message;
+    },
+  },
+};
+</script>
+
